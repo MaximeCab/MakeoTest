157d43724acf255c49e330e728ffaaee9f703c59
MaximeCab <121013656+MaximeCab@users.noreply.github.com>
2023-02-03 11:01:24 +0100

    Update test.php

--- test.php
+++ test.php
@@ -10,33 +10,40 @@
     <!-- mise en page du formulaire en HTML -->
     <form action="#" method="post" name="form" id="form"style='float:left'>
         <label for="nameFirstname">Nom et prénom :</label>
-       <input type="text" name="nameFirstname" id="nameFirstname" placeholder="Entrez votre nom et prénom"><br>
-       <label for="mail">Adresse email :</label>
-       <input type="email" name="mail" id="mail" placeholder="Entrez votre adresse email"><br>
-       <label for="message">Message :</label> <br>
-       <textarea name="message" id="message" cols="30" rows="10" style="resize: none;" placeholder="Entrez votre message..."></textarea><br>
-       <input type="submit" value="Envoyer" name="submit" id="submit">
+            <input type="text" name="nameFirstname" id="nameFirstname" placeholder="Entrez votre nom et prénom"><br>
+            <label for="mail">Adresse email :</label>
+            <input type="email" name="mail" id="mail" placeholder="Entrez votre adresse email"><br>
+            <label for="message">Message :</label> <br>
+            <textarea name="message" id="message" cols="30" rows="10" style="resize: none;" placeholder="Entrez votre message..."></textarea><br>
+            <input type="submit" value="Envoyer" name="submit" id="submit">
     </form>
 
     <?php
-        $nameFirstname=$_POST['nameFirstname'];
-        $mail =$_POST['mail'];
-        $message =$_POST['message'];
+
         //conexion à la base de données 
         try {
-        $connection = new PDO('mysql:host=localhost;dbname=makeo', 'root', '');
-        $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $connection = new PDO('mysql:host=localhost;dbname=makeo', 'root', '');
+            $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-        print "Erreur !: " . $e->getMessage() . "<br/>";
-        die();
-         }
-        //traitement et envoie a la base de données
-         $insert ="INSERT INTO infos VALUES ('$nameFirstname','$mail','$message') ";
-         $result = $connection->exec($insert);
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+
+        if (isset($_REQUEST['submit'] )) {
+            $nameFirstname = $_POST['nameFirstname'];
+            $mail = $_POST['mail'];
+            $message = $_POST['message'];
+            
+            //traitement et envoie a la base de données
+            $insert = "INSERT INTO infos VALUES ('$nameFirstname','$mail','$message')";
+            $result = $connection->exec($insert);
+
+        }
+
         // affichage des infos de la base de données
         $select= "SELECT * FROM infos ";
         $result= $connection->query($select);
-         
+        
         if ($result->rowCount()>0) {
             echo "<table style='float:right'>";
             echo "<tr>";
@@ -53,13 +60,11 @@
             }
             echo "</table>";
         }
-        //fermeture de la connexion à la base de données
-         $connection = null;
-
         
-
-         
-         ?>
+        //fermeture de la connexion à la base de données
+        $connection = null;
+        
+        ?>
     
 </body>
 </html>
